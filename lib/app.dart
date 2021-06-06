dce9bdc8b87efe068bd6d1beacf5e80eac5fa833
feritkartalcoen <53876311+feritkartalcoen@users.noreply.github.com>
2021-06-06 20:56:23 +0300

    Page transition theme changed

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:takasburada/pages/home.dart';
 import 'package:takasburada/pages/login.dart';
@@ -9,6 +10,18 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: FadeThroughPageTransitionsBuilder(),
+            TargetPlatform.fuchsia: FadeThroughPageTransitionsBuilder(),
+            TargetPlatform.iOS: FadeThroughPageTransitionsBuilder(),
+            TargetPlatform.linux: FadeThroughPageTransitionsBuilder(),
+            TargetPlatform.macOS: FadeThroughPageTransitionsBuilder(),
+            TargetPlatform.windows: FadeThroughPageTransitionsBuilder(),
+          },
+        ),
+      ),
       routes: {
         "/": (context) => Login(),
         "/home": (context) => Home(),
