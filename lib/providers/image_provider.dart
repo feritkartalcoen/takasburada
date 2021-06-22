33ab42e54fcf79c8a47f3103ee559d43bf18a295
feritkartalcoen <53876311+feritkartalcoen@users.noreply.github.com>
2021-06-22 12:54:25 +0300

    Image Cropper added

--- lib/providers/image_provider.dart
+++ lib/providers/image_provider.dart
@@ -1,17 +1,28 @@
 import 'dart:io';
 
 import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 class ImageProvider {
   ImagePicker imagePicker = ImagePicker();
 
   Future<File?> selectImage() async {
-    File file;
+    File? file;
     try {
       PickedFile? pickedFile = await imagePicker.getImage(source: ImageSource.gallery);
-      file = File(pickedFile!.path);
-      print("image selected");
-      return file;
+      try {
+        file = await ImageCropper.cropImage(
+          sourcePath: pickedFile!.path,
+          aspectRatioPresets: [
+            CropAspectRatioPreset.square
+          ],
+        );
+        print("image selected");
+        return file;
+      } catch (e) {
+        print("no image selected");
+        return null;
+      }
     } catch (e) {
       print("no image selected");
       return null;
