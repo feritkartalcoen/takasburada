b7bb69d665f7f9718ce806833ac7af9fb9bf7cc0
feritkartalcoen <53876311+feritkartalcoen@users.noreply.github.com>
2021-06-26 11:38:26 +0300

    Result page improved

--- lib/pages/result.dart
+++ lib/pages/result.dart
@@ -1,13 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart' hide AppBar, IconButton, TextButton;
 import 'package:takasburada/models/ad.dart';
 import 'package:takasburada/constants/constants.dart';
 import 'package:takasburada/constants/custom_icons.dart';
+import 'package:takasburada/models/product.dart';
+import 'package:takasburada/models/user.dart';
 import 'package:takasburada/widgets/ad_tile.dart';
 import 'package:takasburada/widgets/app_bar.dart';
 import 'package:takasburada/widgets/icon_button.dart';
 import 'package:takasburada/widgets/text_button.dart';
-import 'package:provider/provider.dart';
-import 'package:takasburada/providers/providers.dart' as providers;
 
 class Result extends StatelessWidget {
   final VoidCallback? onTap;
@@ -18,7 +19,6 @@ class Result extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String currentUserId = context.read<providers.FirebaseProvider>().firebaseAuth.currentUser!.uid;
     return Scaffold(
       appBar: AppBar(
         withTitle: false,
@@ -29,28 +29,53 @@ class Result extends StatelessWidget {
           TextButton(text: "sort", onTap: () {}),
         ],
       ),
-      body: FutureBuilder<List<Ad>>(
-        future: context.read<providers.FirebaseProvider>().getAds(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            List<Ad> ads = snapshot.data!.where((ad) => ad.userId != currentUserId).toList();
-            return ListView.separated(
-              physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-              padding: EdgeInsets.zero,
-              itemCount: ads.length,
-              itemBuilder: (context, index) {
-                return AdTile(
-                  ad: ads[index],
-                );
-              },
-              separatorBuilder: (context, index) {
-                return SizedBox(
-                  height: containerPadding,
-                );
-              },
-            );
+      body: StreamBuilder<QuerySnapshot<Ad>>(
+        stream: Ad.getAds(),
+        builder: (context, adsSnapshot) {
+          if (adsSnapshot.hasError) {
+            return SizedBox();
           }
-          return SizedBox();
+          if (adsSnapshot.connectionState == ConnectionState.waiting) {
+            return SizedBox();
+          }
+          List<Ad> ads = adsSnapshot.data!.docs.map((document) => document.data()).toList().where((ad) => ad.userId != User.currentUserId).toList();
+          return ads != []
+              ? ListView.separated(
+                  physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                  padding: EdgeInsets.zero,
+                  itemCount: ads.length,
+                  itemBuilder: (context, index) {
+                    Ad ad = ads[index];
+                    return StreamBuilder<QuerySnapshot<Product>>(
+                      stream: Product.getProducts(adId: ad.id),
+                      builder: (context, productsSnapshot) {
+                        if (productsSnapshot.hasError) {
+                          return SizedBox();
+                        }
+                        if (productsSnapshot.connectionState == ConnectionState.waiting) {
+                          return SizedBox();
+                        }
+                        ad.products = productsSnapshot.data!.docs.map((document) {
+                          return Product(
+                            id: document["id"],
+                            name: document["name"],
+                            photo: document["photo"],
+                            isGiven: document["isGiven"],
+                          );
+                        }).toList();
+                        return AdTile(
+                          ad: ad,
+                        );
+                      },
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(
+                      height: containerPadding,
+                    );
+                  },
+                )
+              : SizedBox();
         },
       ),
     );
